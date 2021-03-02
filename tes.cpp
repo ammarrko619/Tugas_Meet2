267541fa83e2f028dbcaeec6f1d4df1a2fd483eb
sentotalibasa <50816373+sentotalibasa@users.noreply.github.com>
2021-03-03 00:15:56 +0700

    Update tes.cpp

--- tes.cpp
+++ tes.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 int main()
 {
-	int a, b, hasil;
-	a = 3;
-	b = 2;
-	hasil = a+b;
-	cout<<hasil;
+	int a, b, tambah,kurang, kali, bagi;
+	a=4;
+	b=2;
+	tambah = a+b;
+	kurang=a-b;
+	kali=a*b;
+	bagi=a/b;
+	cout<<"a="<<a<<" dan b="<<b<<endl;
+	cout<<"hasil tambah : "<<tambah<<endl;
+	cout<<"hasil kurang : "<<kurang<<endl;
+	cout<<"hasil kali : "<<kali<<endl;
+	cout<<"hasil bagi : " <<bagi<<endl;
 	return 0;
 }
