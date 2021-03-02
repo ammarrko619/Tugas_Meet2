634dbec6eaccf019ba1085d82de528c366bf5366
sentotalibasa <50816373+sentotalibasa@users.noreply.github.com>
2021-03-03 00:24:10 +0700

    Update tes.cpp

--- tes.cpp
+++ tes.cpp
@@ -6,10 +6,13 @@ int main()
 	a=4;
 	b=2;
 	tambah = a+b;
-	
+	kurang=a-b;
+	kali=a*b;
+	bagi=a/b;
 	cout<<"a="<<a<<" dan b="<<b<<endl;
-	
 	cout<<"hasil tambah : "<<tambah<<endl;
-	
+	cout<<"hasil kurang : "<<kurang<<endl;
+	cout<<"hasil kali : "<<kali<<endl;
+	cout<<"hasil bagi : " <<bagi<<endl;
 	return 0;
 }
